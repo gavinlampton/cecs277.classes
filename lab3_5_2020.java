f9fa19890401b3200801f31a124ae3e72f947147
Gavin Lampton <60271526+gavinlampton@users.noreply.github.com>
2020-03-05 16:24:14 -0800

    Add files via upload

--- lab3_5_2020.java
+++ lab3_5_2020.java
@@ -1,22 +1,38 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.File;
+
 class lab3_5_2020
 {
 	public static void main(String[] args)
 	{
-		InputHandler in;
-		if(args.length==1)
+		InputHandler mIn;
+		PrintWriter mOut;
+		
+		if(args.length==2)
 		{
-			in = new InputHandler(args[0]);
+			mIn = new InputHandler(args[0]);
 			
-			System.out.printf("File: %s\n",args[0]);
+			System.out.printf("File: %s\nOut: %s",args[0],args[1]);
 			
-			if(in.isFile())
+			if(mIn.isFile())
 			{
-				while(in.hasInput())
+				try
 				{
-					System.out.println(in.takeInput());
+					mOut = new PrintWriter(args[1]);
+				}
+				catch(java.io.FileNotFoundException e)
+				{
+					mOut = new PrintWriter(System.out);
 				}
 				
-				in.close();
+				while(mIn.hasInput())
+				{
+						mOut.println(mIn.getInput());
+				}
+				
+				mIn.close();
+				mOut.close();
 			}
 			else
 			{
